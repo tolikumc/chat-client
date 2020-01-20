9354a7eb1833ea00128df990338ead1e23ce41aa
vitaliy <tolikumc@gmail.com>
2020-01-20 20:31:57 +0200

    Create empty dialog

--- src/components/home/dialog-list/index.js
+++ src/components/home/dialog-list/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './index.scss';
 import { Icon } from 'antd';
-import { Message } from '../message';
 import { Status } from '../status';
 import { ChatInput } from '../chat-input';
+import { Messages } from '../messages';
 
 export const DialogsList = () => (
   <div className="chat__dialog">
@@ -15,90 +15,10 @@ export const DialogsList = () => (
           <Status online={true} />
         </div>
       </div>
-      <Icon type="ellipsis" style={{ fontSize: '25px', }} />
+      <Icon type="ellipsis" style={{ fontSize: '25px' }} />
     </div>
     <div className="chat__dialog-message">
-      <Message
-        date="Wed Jan 15 2020 14:58:35"
-        isMe={false}
-        audio={
-          'https://notificationsounds.com/soundfiles/c361bc7b2c033a83d663b8d9fb4be56e/file-sounds-1146-quest.mp3'
-        }
-        avatar={`https://images.unsplash.com/photo-1578021122639-7af4e73ba117?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60`}
-      />
-      <Message
-        date="Wed Jan 13 2020 14:58:35"
-        isMe={true}
-        attachments={[
-          {
-            filename: 'name',
-            url: 'https://source.unsplash.com/collection/190727/100x100'
-          },
-          {
-            filename: 'name',
-            url: 'https://source.unsplash.com/collection/190727/100x100'
-          },
-          {
-            filename: 'name',
-            url: 'https://source.unsplash.com/collection/190727/100x100'
-          }
-        ]}
-        isRead={true}
-        text={'Салам, Брут! Чё, как, уничтожил флот галлов?'}
-        avatar={`https://images.unsplash.com/photo-1578021122639-7af4e73ba117?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60`}
-      />
-      <Message
-        date="Wed Jan 13 2020 14:58:35"
-        isMe={true}
-        isRead={true}
-        text={'Салам, Брут! Чё, как, уничтожил флот галлов?'}
-        avatar={`https://images.unsplash.com/photo-1578021122639-7af4e73ba117?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60`}
-      />
-      <Message
-        date="Wed Jan 13 2020 14:58:35"
-        isMe={true}
-        isRead={true}
-        text={'Салам, Брут! Чё, как, уничтожил флот галлов?'}
-        avatar={`https://images.unsplash.com/photo-1578021122639-7af4e73ba117?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60`}
-      />{' '}
-      <Message
-        date="Wed Jan 13 2020 14:58:35"
-        isMe={true}
-        isRead={true}
-        text={'Салам, Брут! Чё, как, уничтожил флот галлов?'}
-        avatar={`https://images.unsplash.com/photo-1578021122639-7af4e73ba117?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60`}
-      />
-      <Message
-        date="Wed Jan 13 2020 14:58:35"
-        isMe={true}
-        isRead={true}
-        text={'Салам, Брут! Чё, как, уничтожил флот галлов?'}
-        avatar={`https://images.unsplash.com/photo-1578021122639-7af4e73ba117?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60`}
-      />{' '}
-      <Message
-        date="Wed Jan 13 2020 14:58:35"
-        isMe={true}
-        isRead={true}
-        text={'Салам, Брут! Чё, как, уничтожил флот галлов?'}
-        avatar={`https://images.unsplash.com/photo-1578021122639-7af4e73ba117?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60`}
-      />{' '}
-      <Message
-        date="Wed Jan 13 2020 14:58:35"
-        isMe={true}
-        isRead={true}
-        text={'Салам, Брут! Чё, как, уничтожил флот галлов?'}
-        avatar={`https://images.unsplash.com/photo-1578021122639-7af4e73ba117?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60`}
-      />
-      <Message
-        isMe={false}
-        attachments={[
-          {
-            filename: 'name',
-            url: 'https://source.unsplash.com/collection/190727/100x100'
-          }
-        ]}
-        avatar={`https://images.unsplash.com/photo-1578021122639-7af4e73ba117?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60`}
-      />
+      <Messages />
     </div>
     <div className="chat__dialog-input">
       <ChatInput />
