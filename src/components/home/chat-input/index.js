3742d90e3399e049587232141ae0a9655fb06f76
vitaliy <tolikumc@gmail.com>
2020-01-29 17:43:12 +0200

    Add 'smile' icon functionality

--- src/components/home/chat-input/index.js
+++ src/components/home/chat-input/index.js
@@ -2,13 +2,21 @@ import React, { useState } from 'react';
 import './index.scss';
 import { Icon, Input } from 'antd';
 import { UploadField } from '@navjobs/upload';
+import { Picker } from 'emoji-mart';
 
 export const ChatInput = () => {
   const [value, setValue] = useState('');
+  const [emojiVisible, setEmojiVisible] = useState(false);
   return (
     <div className="chat-input">
       <div className="chat-input-smile">
-        <Icon type="smile" />
+        {emojiVisible && (
+          <div className="chat-input-emoji-picker">
+            <Picker set="emojione" />
+          </div>
+        )}
+
+        <Icon type="smile" onClick={() => setEmojiVisible(!emojiVisible)} />
       </div>
       <Input onChange={e => setValue(e.target.value)} size="large" />
       <div className="chat-input-actions">
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.scss';
+import 'emoji-mart/css/emoji-mart.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
