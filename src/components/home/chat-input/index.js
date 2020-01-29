37420869cc0b2255649075c7cdc2dfa07dbafd74
vitaliy <tolikumc@gmail.com>
2020-01-29 17:09:14 +0200

    Add 'camera' icon functionality

--- src/components/home/chat-input/index.js
+++ src/components/home/chat-input/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './index.scss';
 import { Icon, Input } from 'antd';
+import { UploadField } from '@navjobs/upload';
 
 export const ChatInput = () => {
   const [value, setValue] = useState('');
@@ -11,7 +12,19 @@ export const ChatInput = () => {
       </div>
       <Input onChange={e => setValue(e.target.value)} size="large" />
       <div className="chat-input-actions">
-        <Icon type="camera" />
+        <UploadField
+          onFiles={files => console.log(files)}
+          containerProps={{
+            className: 'chat-input-actions-upload-btn'
+          }}
+          uploadProps={{
+            accept: '.jpeg, .png, .jpg, .gif, .bmp',
+            multiple: 'multiple'
+          }}
+        >
+          <Icon type="camera" />
+        </UploadField>
+
         {!value ? <Icon type="audio" /> : <Icon type="check-circle" />}
       </div>
     </div>
